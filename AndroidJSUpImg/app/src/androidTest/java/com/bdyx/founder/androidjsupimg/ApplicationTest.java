691d2b90874fc9cb53a3ad58a5fd900fa55a00f2
weifengzz <weifengzz@outlook.com>
2016-06-25 10:36:23 +0800

    多图选择器

---
+++ AndroidJSUpImg/app/src/androidTest/java/com/bdyx/founder/androidjsupimg/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.bdyx.founder.androidjsupimg;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
